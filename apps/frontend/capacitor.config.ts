96d68b0d3dcc501576ef62ad8db172d3c0f0ded0
Locacoeur <support@locacoeur.com>
2026-06-09 15:12:49 +0200

    Adding capacitor

---
+++ apps/frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.locacoeur.agents',
+  appName: 'frontend',
+  webDir: 'dist'
+};
+
+export default config;
